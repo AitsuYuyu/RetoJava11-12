1870e00660f53a33e2150f0aca80b25af9b8e9c9
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-12-11 17:01:38 -0500

    feat: :sparkles: add second point from exercise

---
+++ exerciseNum3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class exerciseNum3 {
+      public static void main(String[] args) {
+          // Escribe un programa que generede forma aleatorea contrasenyas, que puedas configurar.
+          
+          // Ni idea alkjskaljslkaj
+      }
+}
