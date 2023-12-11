52d48b53453dbdf9ceade0f479e1ef59e79479fe
AitsuYuyu <136203413+AitsuYuyu@users.noreply.github.com>
2023-12-11 17:02:17 -0500

    feat: sparkles: add fourth point from activity

---
+++ exerciseNum4.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class exerciseNum4 {
+      public static void main(String[] args) {
+          // apartir del codigo de python dado, realiza una explicacion del funcionamiento y una traduccion del lenguaje java
+          
+          // Una explicacion corta de este ejercisio es que se crean dos variables (a y b) con un tipo de valor cada una (0 y 1), lo q hace esta pequeña funcionalidad es crear un ciclo con for para crear un rango de 1 10 numeros, en donde imprime "a" y cuando termine, acabe con un espacio, luego hace una operacion de a y b que sea igual a b, a mas b, entonces imprmie.. nada? xddd
+          
+          
+          // traduccion al lenguaje java
+          
+          int a = 0;
+          int b = 1;
+          
+          
+          for (.range(10));
+          // ni idea xddd
+      }
+}
